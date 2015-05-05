122e9e851b6cfa7383ffbaf0d3833faad6f5f9a9
Elias Dorneles <eliasdorneles@gmail.com>
2015-05-04 22:31:16 -0300

    initial state is follower

--- fuzzyraft/fuzzyraft.py
+++ fuzzyraft/fuzzyraft.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+
+
+class State:
+    FOLLOWER = 0
+    CANDIDATE = 1
+    LEADER = 2
+
+
+class Node(object):
+    def __init__(self):
+        self.state = State.FOLLOWER
--- tests/test_fuzzyraft.py
+++ tests/test_fuzzyraft.py
@@ -14,15 +14,9 @@ from fuzzyraft import fuzzyraft
 
 
 class TestFuzzyraft(unittest.TestCase):
-
-    def setUp(self):
-        pass
-
-    def test_something(self):
-        pass
-
-    def tearDown(self):
-        pass
+    def test_node_initial_state_is_follower(self):
+        node = fuzzyraft.Node()
+        self.assertEquals(node.state, fuzzyraft.State.FOLLOWER)
 
 if __name__ == '__main__':
     unittest.main()
